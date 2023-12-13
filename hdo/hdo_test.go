b8952332692ca977db39d190fa33a6c3dc3fb8b4
MiaoJingyi <miaojingyi86@gmail.com>
2023-12-14 00:57:16 +0800

    .

--- hdo/hdo_test.go
+++ hdo/hdo_test.go
@@ -12,9 +12,10 @@ func TestDo(t *testing.T) {
 	nums := []int{8, 5, 8, 3}
 
 	for i := int64(0); i < math.MaxInt64; i++ {
-		log.Printf("使用随机种子【%d】\n", i)
-		rand.Seed(i)
-		tbl := rand.Perm(math.MaxInt32)
+		tbl := make([]int, len(nums)+3)
+		for p := 0; p < len(nums)+3; p++ {
+			tbl = append(tbl, rand.Intn(10))
+		}
 
 		for k := 0; k < len(tbl); k++ {
 			if k+len(nums) > len(tbl) {
@@ -29,8 +30,8 @@ func TestDo(t *testing.T) {
 				}
 			}
 
-			if isMatch && len(tbl)-k > 3 {
-				log.Printf("\t随机种子【%d】，相同的起始下标索引为【%d】，后3位【%d,%d,%d】\n", i, k, tbl[k+len(nums)], tbl[k+len(nums)+1], tbl[k+len(nums)+2])
+			if isMatch && k+len(nums)+2 < len(tbl)-1 {
+				log.Printf("\t相同的起始下标索引为【%d】，后3位【%d,%d,%d】\n", k, tbl[k+len(nums)], tbl[k+len(nums)+1], tbl[k+len(nums)+2])
 			}
 		}
 	}
