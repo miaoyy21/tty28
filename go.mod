576b61aab9af5909a1263c6d43eac7dbad36b761
苗景译 <miaojingyi86@gmail.com>
2024-10-14 17:42:01 +0800

    .

--- go.mod
+++ go.mod
@@ -2,16 +2,9 @@ module tty28
 
 go 1.19
 
-require (
-	github.com/go-sql-driver/mysql v1.7.1
-	google.golang.org/grpc v1.59.0
-	google.golang.org/protobuf v1.31.0
-)
+require github.com/PuerkitoBio/goquery v1.9.3
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.29.0 // indirect
 )
--- huiwan28/q_charts.go
+++ huiwan28/q_charts.go
@@ -2,15 +2,67 @@ package huiwan28
 
 import (
 	"bytes"
+	"github.com/PuerkitoBio/goquery"
 	"log"
+	"strconv"
 )
 
-func qCharts() {
+func qCharts() (spaces map[int32]float64, result int32, issue int64, err error) {
 	buf := new(bytes.Buffer)
 
 	if err := Do("huiwan28_charts", map[string]string{}, nil, buf); err != nil {
-		return
+		return spaces, result, issue, err
 	}
 
-	log.Printf("Charts is %s", buf.String())
+	doc, err := goquery.NewDocumentFromReader(buf)
+	if err != nil {
+		return spaces, result, issue, err
+	}
+
+	// 获取开奖间隔系数、最新开奖结果和最新开奖期数
+	doc.Find("table").Each(func(iTable int, sTable *goquery.Selection) {
+		sTable.Find("tr").Each(func(itr int, str *goquery.Selection) {
+			switch itr {
+			case 1:
+				str.Find("th").Each(func(ith int, sth *goquery.Selection) {
+					if ith == 0 || ith >= 28 {
+						return
+					}
+
+					f64, err := strconv.ParseFloat(sth.Text(), 32)
+					if err != nil {
+						log.Panicf("strconv.ParseFloat() Failure :: %s", err.Error())
+						return
+					}
+
+					num := int32(ith) - 1
+					spaces[num] = f64 / (1000 / float64(STDS1000[num]))
+
+					if f64 == 0 {
+						result = num
+					}
+				})
+			case 5:
+				str.Find("td").Each(func(itd int, std *goquery.Selection) {
+					if itd != 0 {
+						return
+					}
+
+					i64, err := strconv.ParseInt(std.Text(), 10, 64)
+					if err != nil {
+						log.Panicf("strconv.ParseInt() Failure :: %s", err.Error())
+					}
+
+					issue = i64
+				})
+			}
+		})
+	})
+
+	// 获取最新开奖期数
+	log.Printf("实际间隔：%v\n", spaces)
+	log.Printf("最新开奖结果：%d\n", result)
+	log.Printf("最新开奖期数：%d\n", issue)
+
+	return spaces, result, issue, nil
 }
